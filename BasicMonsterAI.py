c37e6e3988053fa86baddf63feab08b653bbc817
David Larson <ernfrid@gmail.com>
2013-02-23 20:42:06 -0600

    remove old code

--- BasicMonsterAI.py
+++
@@ -1,9 +0,0 @@
-import libtcodpy as libtcod
-class BasicMonsterAI:
-    def take_turn(self,player, map, fov_map):
-        monster = self.owner
-        if libtcod.map_is_in_fov(fov_map, monster.x, monster.y):
-            if monster.distance_to(player) >= 2:
-                monster.move_towards(map, player.x, player.y)
-            elif player.phenotype.hp > 0:
-                monster.phenotype.attack(player)
