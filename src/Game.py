b9e3b4d8ee7b1af578e467bb274ee54bcdad6a9f
David Larson <ernfrid@gmail.com>
2013-02-23 20:45:45 -0600

    gut game class

---
+++ src/Game.py
@@ -0,0 +1,13 @@
+
+class Game:
+    """The Esotera game engine"""
+
+    def __init__(self,title):
+        pass 
+    
+    def new_game(self):
+        pass
+
+    def start(self):
+        pass
+
---
+++ src/esotera.py
@@ -0,0 +1,6 @@
+from Game import Game
+
+#Start the game here
+esotera = Game('Esotera Demo')
+esotera.new_game()
+
