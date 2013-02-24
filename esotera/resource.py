460dfa1dd17f67fc19ff8469a9d7de1c6623ee17
David Larson <ernfrid@gmail.com>
2013-02-24 16:24:59 -0600

    inital code and tests for resources.

---
+++ esotera/resource.py
@@ -0,0 +1,7 @@
+class Resource:
+    """The Resource class serves to represent game resources. Resources are items required or desired by actors.
+    """
+    def __init__(self,type,quantity):
+        self.type = type
+        self.quantity = quantity
+
---
+++ test/test_resource.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+"""Unit tests for the Resource class"""
+
+from unittest import TestCase
+from esotera.resource import Resource 
+
+class TestEvent(TestCase):
+    def test_creation(self):
+        resource = Resource('food',10)
+        self.assertEqual(resource.type,'food')
+        self.assertEqual(resource.quantity, 10)
+    def test_addition(self):
+        res1 = Resource('food',1)
+        res2 = Resource('food',1)
+        sum = res1 + res2
+        self.assertEqual(sum.quantity, 2)
+    def test_numeric_addition(self):
+        res1 = Resource('food',1)
+        sum = res1 + 1
+        self.assertEqual(sum.quantity, 2)
+    def test_numeric_raddition(self):
+        res1 = Resource('food',1)
+        sum = 1 + res1 
+        self.assertEqual(sum.quantity, 2)
+    def test_subtraction(self):
+        res1 = Resource('food',1)
+        res2 = Resource('food',1)
+        sum = res1 - res2
+        self.assertEqual(sum.quantity, 0)
+    def test_numeric_subtraction(self):
+        res1 = Resource('food',1)
+        sum = res1 - 1
+        self.assertEqual(sum.quantity, 0)
+    def test_numeric_rsubtraction(self):
+        res1 = Resource('food',1)
+        sum = 0 - res1 
+        self.assertEqual(sum.quantity, 0)
+    def test_multiplication(self):
+        res1 = Resource('food',1)
+        res2 = Resource('food',1)
+        result = res1 * res2
+        self.assertEqual(result.quantity, 1)
+    def test_division(self):
+        res1 = Resource('food',1)
+        sum = res1 / 2
+        self.assertEqual(sum.quantity, 0)
+    def test_add_type_incompatibility(self):
+        res1 = Resource('food',1)
+        res2 = Resource('mate',1)
+        with self.assertRaises(TypeError):
+            res1 + res2
+    def test_sub_type_incompatibility(self):
+        res1 = Resource('food',1)
+        res2 = Resource('mate',1)
+        with self.assertRaises(TypeError):
+            res1 - res2
+    def test_mul_type_incompatibility(self):
+        res1 = Resource('food',1)
+        res2 = Resource('mate',1)
+        with self.assertRaises(TypeError):
+            res1 * res2
+    def test_floordiv_type_incompatibility(self):
+        res1 = Resource('food',1)
+        res2 = Resource('mate',1)
+        with self.assertRaises(TypeError):
+            res1 // res2
+
