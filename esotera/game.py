f33e2a51aec2295314f3e7a0dcc61f1fa140ee72
David Larson <ernfrid@gmail.com>
2013-02-24 15:38:09 -0600

    some simple stubins of datastructures

--- esotera/game.py
+++ esotera/game.py
@@ -4,9 +4,13 @@ class Game:
     """The Esotera game engine"""
 
     def __init__(self):
+        """This initializes the datastructures to be used in the game"""
         self.event_queue = deque() 
+        self.actors = list()
+        self.resources = list()
     
     def new_game(self):
+        """This populates the game world and starts the game"""
         pass
 
     def start(self):
