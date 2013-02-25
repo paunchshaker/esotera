ff47e4130207a8cc35ce5b1f5a76839ec3190672
David Larson <ernfrid@gmail.com>
2013-02-24 22:24:25 -0600

    add docstrings

--- esotera/actor.py
+++ esotera/actor.py
@@ -5,12 +5,14 @@ class Actor:
     number = 0
 
     def __init__(self, name = None):
+        """Initialize a new Actor."""
         if name:
             self.name = name
         else:
             Actor.number += 1
             self.name = "Actor" + str(Actor.number)
     def offer(self, target, give, receive):
+        """Offer an exchange (possibly one-sided) of resources."""
         accepts = target.accept(source = self, give = receive, receive = give)
         if accepts:
             self.exchange(target = target, give = give, receive = receive)
@@ -18,8 +20,10 @@ class Actor:
             #offer rejected, update AI accordingly
             pass
     def exchange(self, target, give, receive):
+        """Exchange resources between Actors."""
         #exchange resources here
         pass
     def accept(self, source, give, receive):
+        """Decide whether to accept or reject an offer"""
         #go through AI to make decision, update AI here
         pass
