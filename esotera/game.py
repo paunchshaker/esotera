486e77b1c5f04f8f56fd48178ff6b3495bca1e67
David Larson <ernfrid@gmail.com>
2013-02-24 22:31:14 -0600

    clean up some docstrings

--- esotera/game.py
+++ esotera/game.py
@@ -4,15 +4,16 @@ class Game:
     """The Esotera game engine"""
 
     def __init__(self):
-        """This initializes the datastructures to be used in the game"""
+        """Initialize the datastructures to be used in the game."""
         self.event_queue = deque() 
         self.actors = list()
         self.resources = list()
     
     def new_game(self):
-        """This populates the game world and starts the game"""
+        """Populates the game world and start the game"""
         pass
 
     def start(self):
+        """Start the game."""
         pass
 
